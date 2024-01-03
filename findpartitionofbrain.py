d1c5c16d2ba3e908a5d1357ff9dd4bb5d199c586
mich3leRusso <109308928+mich3leRusso@users.noreply.github.com>
2024-01-03 01:17:23 +0100

    Update findpartitionofbrain.py
    error found and fixed

--- findpartitionofbrain.py
+++ findpartitionofbrain.py
@@ -36,7 +36,7 @@ def compute_communities(dataframe, condition):
           com= com + greedy_modularity_communities(component_subgraph)
         ls=community_metrics(G,com)
         #Put results in the result df
-        results.append((row.name,( com,ls)))
+        results.append((row.name, com,ls))
     return results
 
 #computes closeness centrality and betweenness centrality fo each module
@@ -47,5 +47,5 @@ def community_metrics(G,com):
         subgraph = G.subgraph(comm)
         betweenness_centrality = nx.betweenness_centrality(subgraph)
         closeness_centrality = nx.closeness_centrality(subgraph)
-        ls.append(betweenness_centrality,closeness_centrality)
+        ls.append((betweenness_centrality,closeness_centrality))
     return ls
